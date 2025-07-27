3aad38c3022ee3e3896b9e98af5adddaae502360
Anas BenOmar <138506861+AnsBenO@users.noreply.github.com>
2025-07-27 18:05:32 +0100

    style improvements: dark / light mode and other improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,58 @@
-import { Component, inject, model, ModelSignal } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  model,
+  ModelSignal,
+  signal,
+} from '@angular/core';
 import { PlayerComponent } from './components/player/player.component';
 import { FilterPipe } from './filter.pipe';
 import { FormsModule } from '@angular/forms';
 import { PlayerService } from './services/player.service';
 import { AsyncPipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+type Theme = 'dark' | 'light' | null;
 
 @Component({
   selector: 'app-root',
-  imports: [PlayerComponent, FilterPipe, FormsModule, AsyncPipe],
+  imports: [
+    PlayerComponent,
+    FilterPipe,
+    FormsModule,
+    AsyncPipe,
+    FontAwesomeModule,
+  ],
   providers: [PlayerService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cdk-demo';
-  searchText: ModelSignal<string> = model('');
   playerService = inject(PlayerService);
+  darkIcon = faMoon;
+  lightIcon = faSun;
+  searchText: ModelSignal<string> = model('');
+  theme = signal<Theme>(null);
+  themeEffect = effect(() => this.applyTheme(this.theme()));
+
+  constructor() {
+    const defaultTheme = localStorage.getItem('theme') as
+      | 'dark'
+      | 'light'
+      | null;
+    const appliedTheme = defaultTheme || 'light';
+    this.theme.set(appliedTheme);
+    document.body.setAttribute('data-theme', appliedTheme);
+  }
+
+  applyTheme(t: Theme) {
+    if (!t) {
+      return;
+    }
+    document.body.setAttribute('data-theme', t as string);
+    localStorage.setItem('theme', t as string);
+  }
 }
